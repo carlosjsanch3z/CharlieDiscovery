20a291c0c7028e4156ba881fdfb83c96f713ca7b
Carlos Jesús Sánchez <carlosjsanchezortega@gmail.com>
2016-05-12 10:58:35 +0200

    Ficheros nuevos

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -6,8 +6,12 @@ def nameindex(name='Stranger'):
  
 @route('/')
 def index():
-    return '<strong>Hello World!</strong>'
+    return template('index2.tpl')
 
+# Contenido Estatico
+@route('/static/<filepath:path>')
+def server_static(filepath):
+    return static_file(filepath, root=os.environ['OPENSHIFT_REPO_DIR']+"wsgi/static")
 # This must be added in order to do correct path lookups for the views
 import os
 from bottle import TEMPLATE_PATH
