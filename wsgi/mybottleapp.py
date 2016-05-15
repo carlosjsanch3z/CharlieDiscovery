f2c70bdb3adbf32f7f55b1bbe4d4f5159fdba562
Carlos Jesús Sánchez <carlosjsanchezortega@gmail.com>
2016-05-15 16:08:55 +0200

    Cambios

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -2,32 +2,54 @@
 # -*- coding: utf-8 -*-
 
 from bottle import route, default_app, get, post, run, template, error, request, static_file, response
-
+import requests
 
 #Pagina Principal
+
 @route('/')
 def index():
+	'''return 
+	    <form action="/summoner" method="post">
+	        Invocador: <input name="summoner" type="text" />
+	        <br/>
+	        <input value="comprobar" type="submit" />
+	    </form> '''
     return template('index2.tpl')
 
 
 #Pagina donde devuelve la información del jugador
-@route('/', method="POST")
-def get_info():
+
+@route('/summoner', method="POST")
+def infosummoner():
 	APIKey = "30ed66a9-fe04-4b57-ad61-871f1995cfb2"
 	region = "euw"
-	summonerName = str(request.forms.get('summoner'))
-	r = "https://" + region + ".api.pvp.net/api/lol/" + region + "/v1.4/summoner/by-name/" + summonerName + "?api_key=" + APIKey
+	summonerName = request.forms.get('summoner')
+	summonerName = str(summonerName)
+	summonerName = summonerName.lower()
+	#return summonerName
 
-	return template('index.tpl')
-#Si accede a la pagina /summoner sin el metodo POST
+	URL = "https://" + region + ".api.pvp.net/api/lol/" + region + "/v1.4/summoner/by-name/" + summonerName + "?api_key=" + APIKey
 
+	response = requests.get(URL)
+
+	if response.status_code == 200:
+		#Here I return the JSON we just got.
+		responseJSON = response.json()
+		ID = responseJSON[summonerName]['id']
+		ID = str(ID)
+		return return template('index.tpl', identificador=ID, invocador=summonerName)
+	else:
+		notfound = "El nombre de invocador introducido no se encuentra en la region EUW."
+		return template('index2', notfound=notfound)
+
+	#return template('index.tpl', identificador=ID, invocador=summonerName)
 
 
 
 #No se ha encontrado la página
 @error(404)
 def error(error):
-    return "Error 404 papito"
+    return "Error 404"
 
 
 # Contenido Estatico
